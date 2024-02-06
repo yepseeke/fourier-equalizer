e09b1f567bf45a573f072ce4326f57befb79c06d
yepseeke <zolder.kaps@gmail.com>
2024-02-06 19:23:50 +0300

    Added real time spectrum

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 
 from scipy.fft import fft
 from PyQt5 import QtCore, QtWidgets
-from scipy.signal import cheby1
+from scipy import signal
 from scipy.io.wavfile import write
 
-sample_rate = 44100
 
-duration = 0.1
+sample_rate = 44100
+interval = 512
+
+duration = 0.01
 
 
 class SignalPlot(QtWidgets.QMainWindow):
@@ -42,6 +44,43 @@ class SignalPlot(QtWidgets.QMainWindow):
         get_signal = record_signal(duration, sample_rate)
         self.values = moving_average(get_signal.reshape(-1), 20)
         self.line.setData(self.time, self.values)
+
+
+class FFTPlot(QtWidgets.QMainWindow):
+    def __init__(self, sample_rate: int, interval: int, plot_type='log'):
+        super().__init__()
+
+        self.freqs = np.linspace(0, sample_rate // 2, interval // 2)
+        self.values = np.zeros(interval // 2)
+        self.interval = interval
+
+        self.plot_graph = pg.PlotWidget()
+
+        pen = pg.mkPen(color=(1, 1, 1))
+
+        self.setCentralWidget(self.plot_graph)
+        self.plot_graph.setBackground("yellow")
+
+        if plot_type == 'log':
+            self.plot_graph.setLogMode(x=True)
+
+        styles = {"color": "black", "font-size": "15px"}
+        self.plot_graph.setLabel("left", "PSD [dB]", **styles)
+        self.plot_graph.setLabel("bottom", "Frequency [Hz]", **styles)
+
+        self.line = self.plot_graph.plot(self.freqs, self.values, pen=pen)
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+
+    def update_plot(self):
+        get_signal = record_signal(duration, sample_rate).reshape(-1)
+        transformed_signal = signal_fft(get_signal, interval)
+        windowed_fft = transformed_signal * signal.windows.bartlett(interval // 2)
+
+        self.values = windowed_fft # 10 * np.log10(windowed_fft)
+        self.line.setData(self.freqs, self.values)
 
 
 def moving_average(signal, window_size):
@@ -95,12 +134,12 @@ def plot_fft(signal, interval: int, sample_rate: int, axs, plot_type='log'):
 
 
 if __name__ == '__main__':
-    signal = record_signal(duration, sample_rate).reshape(-1)
-    signal_length = signal.shape[0] / sample_rate
-    time_list = np.linspace(0, signal_length, signal.shape[0])
+    sound = record_signal(duration, sample_rate).reshape(-1)
+    signal_length = sound.shape[0] / sample_rate
+    time_list = np.linspace(0, signal_length, sound.shape[0])
 
     app = QtWidgets.QApplication([])
-    main = SignalPlot(time_list, signal)
+    main = FFTPlot(sample_rate, interval)
 
     main.show()
     app.exec()
